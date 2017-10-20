8540c3d1a89f994a1fdd6eea123b9bf98cae9725
George Miller <git@duffleman.co.uk>
2017-10-20 14:33:37 +0100

    stop setting custom backgrounds
    must be in the list

--- public/js/site.js
+++ public/js/site.js
@@ -47,6 +47,9 @@ var app = new Vue({
 			var body = document.body;
 			var css = 'url(' + fileUrl + ')';
 
+			if (!this.backgrounds.includes(file))
+				throw new Error('cannot_change_bg');
+
 			this.saveStorage('background', file);
 
 			this.current = file;
