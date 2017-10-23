81cc309632d6fae6defbe2d78f47c656797852e1
George Miller <git@duffleman.co.uk>
2017-10-23 14:44:06 +0100

    remove vue dev message

--- public/js/vue.js
+++ public/js/vue.js
@@ -8198,11 +8198,11 @@ Vue$3.nextTick(function () {
     config.productionTip !== false &&
     inBrowser && typeof console !== 'undefined'
   ) {
-    console[console.info ? 'info' : 'log'](
-      "You are running Vue in development mode.\n" +
-      "Make sure to turn on production mode when deploying for production.\n" +
-      "See more tips at https://vuejs.org/guide/deployment.html"
-    );
+    // console[console.info ? 'info' : 'log'](
+    //   "You are running Vue in development mode.\n" +
+    //   "Make sure to turn on production mode when deploying for production.\n" +
+    //   "See more tips at https://vuejs.org/guide/deployment.html"
+    // );
   }
 }, 0);
 
